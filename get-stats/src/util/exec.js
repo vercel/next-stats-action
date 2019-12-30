36210065e23089f0f1bc06d71f50015ace47e434
JJ Kasper <jj@jjsweb.site>
2019-12-30 17:56:52 -0600

    Update timeout for milliseconds

--- get-stats/src/util/exec.js
+++ get-stats/src/util/exec.js
@@ -11,7 +11,7 @@ const env = {
 
 function exec(command, noLog = false) {
   if (!noLog) logger(`exec: ${command}`)
-  return execP(command, { env, timeout: 240 })
+  return execP(command, { env, timeout: 180 * 1000 })
 }
 
 exec.spawn = function spawn(command = '', opts = {}) {
