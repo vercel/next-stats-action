51bc5aa89fdb629a8fe915de75d1907c711ce7f2
JJ Kasper <jj@jjsweb.site>
2020-02-18 09:41:58 -0600

    Handle failing to diff file gracefully

--- src/run/collect-diffs.js
+++ src/run/collect-diffs.js
@@ -19,38 +19,40 @@ module.exports = async function collectDiffs(
     // remove any previous files in case they won't be overwritten
     const toRemove = await glob('!(.git)', { cwd: diffingDir, dot: true })
 
-    await Promise.all(toRemove.map(file =>
-      fs.remove(path.join(diffingDir, file))
-    ))
+    await Promise.all(
+      toRemove.map(file => fs.remove(path.join(diffingDir, file)))
+    )
   }
   const diffs = {}
 
-  await Promise.all(filesToTrack.map(async fileGroup => {
-    const { globs } = fileGroup
-    const curFiles = []
+  await Promise.all(
+    filesToTrack.map(async fileGroup => {
+      const { globs } = fileGroup
+      const curFiles = []
 
-    await Promise.all(
-      globs.map(async pattern => {
-        curFiles.push(...(await glob(pattern, { cwd: statsAppDir })))
-      })
-    )
-
-    for (let file of curFiles) {
-      const absPath = path.join(statsAppDir, file)
-
-      const diffDest = path.join(diffingDir, file)
-      await fs.copy(absPath, diffDest)
-    }
-
-    if (curFiles.length > 0) {
-      await exec(
-        `cd ${diffingDir} && ` +
-          `yarn prettier --write ${curFiles
-            .map(f => path.join(diffingDir, f))
-            .join(' ')}`
+      await Promise.all(
+        globs.map(async pattern => {
+          curFiles.push(...(await glob(pattern, { cwd: statsAppDir })))
+        })
       )
-    }
-  }))
+
+      for (let file of curFiles) {
+        const absPath = path.join(statsAppDir, file)
+
+        const diffDest = path.join(diffingDir, file)
+        await fs.copy(absPath, diffDest)
+      }
+
+      if (curFiles.length > 0) {
+        await exec(
+          `cd ${diffingDir} && ` +
+            `yarn prettier --write ${curFiles
+              .map(f => path.join(diffingDir, f))
+              .join(' ')}`
+        )
+      }
+    })
+  )
 
   await exec(`cd ${diffingDir} && git add .`, true)
 
@@ -67,14 +69,16 @@ module.exports = async function collectDiffs(
 
     diffs._renames = []
 
-    await Promise.all(renamedFiles.map(async line => {
-      const [, prev, cur] = line.split('\t')
-      await fs.move(path.join(diffingDir, cur), path.join(diffingDir, prev))
-      diffs._renames.push({
-        prev,
-        cur,
+    await Promise.all(
+      renamedFiles.map(async line => {
+        const [, prev, cur] = line.split('\t')
+        await fs.move(path.join(diffingDir, cur), path.join(diffingDir, prev))
+        diffs._renames.push({
+          prev,
+          cur,
+        })
       })
-    }))
+    )
 
     await exec(`cd ${diffingDir} && git add .`)
 
@@ -83,23 +87,30 @@ module.exports = async function collectDiffs(
     )
     changedFiles = changedFiles.trim().split('\n')
 
-    await Promise.all(changedFiles.map(async file => {
-      const fileKey = path.basename(file)
-      const hasFile = await fs.exists(path.join(diffingDir, file))
+    await Promise.all(
+      changedFiles.map(async file => {
+        const fileKey = path.basename(file)
+        const hasFile = await fs.exists(path.join(diffingDir, file))
 
-      if (!hasFile) {
-        diffs[fileKey] = 'deleted'
-        return
-      }
-      let { stdout } = await exec(
-        `cd ${diffingDir} && git diff --minimal HEAD ${file}`
-      )
-      stdout = (stdout.split(file).pop() || '').trim()
+        if (!hasFile) {
+          diffs[fileKey] = 'deleted'
+          return
+        }
 
-      if (stdout.length > 0) {
-        diffs[fileKey] = stdout
-      }
-    }))
+        try {
+          let { stdout } = await exec(
+            `cd ${diffingDir} && git diff --minimal HEAD ${file}`
+          )
+          stdout = (stdout.split(file).pop() || '').trim()
+          if (stdout.length > 0) {
+            diffs[fileKey] = stdout
+          }
+        } catch (err) {
+          console.error(`Failed to diff ${file}: ${err.message}`)
+          diffs[fileKey] = `failed to diff`
+        }
+      })
+    )
   }
   return diffs
 }
